1d166c49fd2a70f936585a8038658ec668cfcac9
simiaolin <simiao.lin@student.kuleuven.be>
2021-05-15 16:34:02 +0200

    [modify] experiment on ex2.1 iteration numbers on the hopfield  network

--- ex2/rep2.m
+++ ex2/rep2.m
@@ -7,14 +7,26 @@
 T = [1 1; -1 -1; 1 -1]';
 net = newhop(T);
 n=20;
+iteration = 49;
 for i=1:n
     a={rands(2,1)};                     % generate an initial point 
-    [y,Pf,Af] = sim(net,{1 50},{},a);   % simulation of the network for 50 timesteps              
+    b= {[a{1,1}(1,1); -a{1,1}(2,1)]}; 
+   [y,Pf,Af] = sim(net,{1 iteration},{},a);   % simulation of the network for 50 timesteps              
+   [y2, pf, af ] = sim(net, {1 iteration}, {}, b);
     record=[cell2mat(a) cell2mat(y)];   % formatting results  
     start=cell2mat(a);                  % formatting results 
-    plot(start(1,1),start(2,1),'bx',record(1,:),record(2,:),'r'); % plot evolution
+    record_b = [cell2mat(b) cell2mat(y2)];
+    start_b = cell2mat(b);
+    p1 = plot(start(1,1),start(2,1),'bx');
     hold on;
-    plot(record(1,50),record(2,50),'gO');  % plot the final point with a green circle
+    p2 = plot(record(1,:),record(2,:),'r'); % plot evolution
+   
+    
+    p3 = plot(record(1,50),record(2,50),'gO');  % plot the final point with a green circle
+    
+    p4 = plot(start_b(1,1), start_b(2,1), 'bx', record_b(1,:), record_b(2,:), 'r');
+    p5 = plot(record_b(1, 50), record_b(2, 50), 'gO');
+    
 end
-legend('initial state','time evolution','attractor','Location', 'northeast');
+legend([p1 p2 p3] , {'initial state','time evolution','attractor'}, 'Location', 'northeast');
 title('Time evolution in the phase space of 2d Hopfield model');
