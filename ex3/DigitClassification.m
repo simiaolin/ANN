80bee83a6da372a1a20d64f2d4d2fede1d256c77
simiaolin <simiao.lin@student.kuleuven.be>
2021-05-20 10:59:15 +0200

    [add] add time and temporarily remove the normal neural network

--- ex3/DigitClassification.m
+++ ex3/DigitClassification.m
@@ -13,32 +13,43 @@ rng('default')
 load('digittrain_dataset.mat');
 
 % Layer 1
-hiddenSize1 = 100;
+hiddenSize1 = 400;
+hiddenSize2 = 100;
+max_epoch1  = 400;
+max_epoch2  = 200;
+max_epoch3  = 100;
+
+"stack first layer"
+tic
 autoenc1 = trainAutoencoder(xTrainImages,hiddenSize1, ...
-    'MaxEpochs',400, ...
+    'MaxEpochs',max_epoch1, ...
     'L2WeightRegularization',0.004, ...
     'SparsityRegularization',4, ...
     'SparsityProportion',0.15, ...
     'ScaleData', false);
+toc
 
-figure;
-plotWeights(autoenc1);
+% figure;
+% plotWeights(autoenc1);
 feat1 = encode(autoenc1,xTrainImages);
 
 % Layer 2
-hiddenSize2 = 50;
+"stack second layer"
+tic
 autoenc2 = trainAutoencoder(feat1,hiddenSize2, ...
-    'MaxEpochs',100, ...
+    'MaxEpochs',max_epoch2, ...
     'L2WeightRegularization',0.002, ...
     'SparsityRegularization',4, ...
     'SparsityProportion',0.1, ...
     'ScaleData', false);
-
+toc 
 feat2 = encode(autoenc2,feat1);
 
+"stack soft layer"
 % Layer 3
-softnet = trainSoftmaxLayer(feat2,tTrain,'MaxEpochs',400);
-
+tic
+softnet = trainSoftmaxLayer(feat2,tTrain,'MaxEpochs',max_epoch3);
+toc
 
 % Deep Net
 deepnet = stack(autoenc1,autoenc2,softnet);
@@ -54,9 +65,9 @@ for i = 1:numel(xTestImages)
     xTest(:,i) = xTestImages{i}(:);
 end
 y = deepnet(xTest);
-figure;
-plotconfusion(tTest,y);
-classAcc=100*(1-confusion(tTest,y))
+% figure;
+% plotconfusion(tTest,y);
+StackEncodersAcc=100*(1-confusion(tTest,y))
 
 
 % Test fine-tuned deep net
@@ -64,25 +75,28 @@ xTrain = zeros(inputSize,numel(xTrainImages));
 for i = 1:numel(xTrainImages)
     xTrain(:,i) = xTrainImages{i}(:);
 end
+"fine tuned"
+tic 
 deepnet = train(deepnet,xTrain,tTrain);
+toc
 y = deepnet(xTest);
-figure;
-plotconfusion(tTest,y);
-classAcc=100*(1-confusion(tTest,y))
-view(deepnet)
+% figure;
+% plotconfusion(tTest,y);
+FinetunedAcc=100*(1-confusion(tTest,y))
+% view(deepnet)
 
 %Compare with normal neural network (1 hidden layers)
-net = patternnet(100);
-net=train(net,xTrain,tTrain);
-y=net(xTest);
-plotconfusion(tTest,y);
-classAcc=100*(1-confusion(tTest,y))
-view(net)
-
-%Compare with normal neural network (2 hidden layers)
-net = patternnet([100 50]);
-net=train(net,xTrain,tTrain);
-y=net(xTest);
-plotconfusion(tTest,y);
-classAcc=100*(1-confusion(tTest,y))
-view(net)
+% net = patternnet(100);
+% net=train(net,xTrain,tTrain);
+% y=net(xTest);
+% plotconfusion(tTest,y);
+% classAcc=100*(1-confusion(tTest,y))
+% view(net)
+% 
+% %Compare with normal neural network (2 hidden layers)
+% net = patternnet([100 50]);
+% net=train(net,xTrain,tTrain);
+% y=net(xTest);
+% plotconfusion(tTest,y);
+% classAcc=100*(1-confusion(tTest,y))
+% view(net)
